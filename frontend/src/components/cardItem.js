d043dbfb814b511e164688bd2d028f47476cfd92
OverEarth <56574543+OverEarth12@users.noreply.github.com>
2022-04-08 19:08:02 +0700

    component cardItem

--- frontend/src/components/cardItem.js
+++ frontend/src/components/cardItem.js
@@ -1,4 +1,21 @@
 import '../App.css';
-import { Card } from 'react-bootstrap';
+import { Card, Image, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import { useState } from 'react';
+
+export default function CardItem(props){
+    const itemData = props.item
+    console.log(itemData)
+    return(
+        <Card className='Card' style={{padding: 10, flexDirection: "row",width: '50%'}}>
+                                    <Image src={require('../image-placeholder.jpg')} style={{width: "200px", height: "auto"}} />
+                                    <Card.Body style={{textAlign: 'left'}}>
+                                        
+                                        
+                                        <Card.Title>{itemData.name}</Card.Title>
+                                        <Card.Text>{itemData.description}</Card.Text>
+                                        <Button variant="primary">ดูรายละเอียด</Button>
+                                    </Card.Body>
+                                    </Card>
+    );
+}
--- frontend/src/routes/listItem.js
+++ frontend/src/routes/listItem.js
@@ -1,24 +1,37 @@
 import '../App.css';
-import { Card } from 'react-bootstrap';
+import { Card, Container, Image, Row } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import { useState } from 'react';
+import CardItem from '../components/cardItem';
 
 export default function ListItem() {
-  const [items, setItem] = useState([{"name":"Test","description":"Test Item","inStock":3},
-  {"name":"Test","description":"Test Item","inStock":3}])
+  const [items, setItem] = useState([{"name":"1","description":"Test Item","inStock":3},
+  {"name":"2","description":"Test Item","inStock":3}, {"name":"3","description":"Test Item","inStock":3},
+  {"name":"4","description":"Test Item","inStock":3},{"name":"5","description":"Test Item","inStock":3}])
+
+  function renderListcol(list){
+    let itembyrow = [];
+    for (let i = 0; i < list.length; i++) {
+      if(i%2 === 0){
+        itembyrow.push([list[i]])
+      }else{
+        itembyrow[Math.floor(i/2)].push(list[i])
+      }
+    }
+    return itembyrow;
+  } 
+
   return (
     <div className="App">
       <header className="App-header">
         
       </header>
-      <div className="App-body">
-        {items.map((item, index) => <Card className='Card' key={index} style={{marginBottom: 10}}>
-                                    <Card.Body>
-                                      <Card.Img variant="top" src="image-placeholder.jpg" />
-                                      <Card.Title><Link className='postTitle' to="/detail">{item.name}</Link></Card.Title>
-                                      <Card.Text>{item.description}</Card.Text>
-                                    </Card.Body>
-                                    </Card>)}</div>
+      <Container>
+        {renderListcol(items).map((item, index) => <Row key={index} style={{margin: 10}}>
+          <CardItem item={item[0]}/>
+          {item.length > 1 ? <CardItem item={item[1]}/> : <div/>}
+           </Row>)}
+      </Container>
     </div>
   );
 }
