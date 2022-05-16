74f2ca7545e51b7cb22c3925ad53523a4558549e
Tanate62070085 <62070085@it.kmitl.ac.th>
2022-05-16 13:33:22 +0700

    add query
    add query
    - reservation
    edit index.js

--- backend/src/GraphQL/query/query_collection/item.js
+++ backend/src/GraphQL/query/query_collection/item.js
@@ -1,4 +1,8 @@
-import { ItemTC } from "../../../models/item";
+import { schemaComposer } from "graphql-compose";
+import mongooesConnect from "../../../../mongooes-connect";
+import { Itemmodel, ItemTC } from "../../../models/item";
 
 export const item = ItemTC.getResolver("findMany")
 export const itemID = ItemTC.getResolver("findById")
+
+
