c996be5f019b670323b90e2704cb311f785860d3
Tanate62070085 <62070085@it.kmitl.ac.th>
2022-05-17 11:29:11 +0700

    add auto id catagory

--- backend/src/GraphQL/query/query_collection/item.js
+++ backend/src/GraphQL/query/query_collection/item.js
@@ -3,5 +3,5 @@ import { Itemmodel, ItemTC } from "../../../models/item";
 
 export const item = ItemTC.getResolver("findMany")
 export const itemID = ItemTC.getResolver("findById")
-
+export const itemCount = ItemTC.getResolver("count")
 
