25e6fb18ec5037d1bb37b3b4b559682b5d4c3e59
Tanate62070085 <62070085@it.kmitl.ac.th>
2022-05-18 17:57:03 +0700

    Update history.js
    add auto mutation

--- backend/src/GraphQL/mutation/mutation_collection/history.js
+++ backend/src/GraphQL/mutation/mutation_collection/history.js
@@ -1,6 +1,46 @@
-import { HistoryTC } from "../../../models/history";
+import { schemaComposer } from "graphql-compose";
+import { HistoryModel, HistoryTC } from "../../../models/history";
 
-export const createHistory = HistoryTC.getResolver("createOne")
-export const deleteHistory = HistoryTC.getResolver("removeOne")
-export const updateHistory = HistoryTC.getResolver("updateOne")
+export const createHistoryBN = HistoryTC.getResolver("createOne")
+export const deleteHistoryBN = HistoryTC.getResolver("removeOne")
+export const updateHistoryBN = HistoryTC.getResolver("updateOne")
 
+schemaComposer.Mutation.addFields({
+    updateHistory:{
+        type : HistoryTC,
+        args : {
+            ItemCode : "String!",
+        },
+        resolve : async (_, {ItemCode}) => {
+            const HistoryData = await HistoryModel.findOne({itemCode: ItemCode})
+            if (HistoryData){
+                const StatusItem = HistoryData.get("status")
+                if (StatusItem == "borrowing"){
+                    return await HistoryModel.updateOne({
+                        itemCode : ItemCode
+                    },{
+                        status : "returned"
+                    })
+                }
+            }
+            return HistoryData
+        }
+    },
+    createHistory:{
+        type : HistoryTC,
+        args : {
+            username : "String!",
+            itemCode : "String!",
+            status : "String"
+        },
+        resolve : async (_, {username , itemCode, status}) => {
+            if (status == ""){ status = "borrowing" }
+            return await HistoryModel.create({
+                username : username,
+                itemCode : itemCode,
+                status : status
+            })
+        }
+
+    }
+})
